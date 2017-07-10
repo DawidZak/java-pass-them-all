0fa0bce9d3c34717a58ee0302e6af67daae4009c
Dawid Żak <dawix64@gmail.com>
2017-07-10 19:35:47 +0200

    initial commit with 5 tests

---
+++ src/main/java/java8/Stream.java
@@ -0,0 +1,50 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Dawid on 10.07.2017.
+ */
+public class Stream {
+
+
+    public boolean stream_1() {
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        //https://stackoverflow.com/questions/22561614/java-8-streams-min-and-max-why-does-this-compile
+        return 312 == integerList.stream().max(Integer::compare).orElse(1);
+    }
+
+
+    public boolean stream_2(){
+        List<String> strings = Arrays.asList("abc");
+        return "ABC".equals(strings.stream().map(String::toUpperCase).findAny().orElse("a"));
+    }
+
+    //skip one element
+
+    public boolean stream_3(){
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        List<Integer> integerListSkip;
+        integerListSkip = integerList.stream().skip(1).collect(Collectors.toList());
+        return integerList.size()-1 == integerListSkip.size();
+
+    }
+
+
+    public boolean stream_4(){
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        return true == integerList.stream().allMatch( x-> x >= 1);
+
+    }
+
+
+    public boolean stream_5(){
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        Predicate<Integer> p1 = x-> x>=1;
+        return true == integerList.stream().allMatch(p1);
+
+    }
+}
---
+++ src/test/java/java8/Stream.java
@@ -0,0 +1,59 @@
+package java8;
+
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Dawid on 08.07.2017.
+ */
+public class Stream {
+
+    @Test
+    public void stream_1() {
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        //https://stackoverflow.com/questions/22561614/java-8-streams-min-and-max-why-does-this-compile
+
+        assertEquals(312,2); // replace 2 with expression
+
+
+    }
+
+    @Test
+    public void stream_2(){
+        List<String> strings = Arrays.asList("abc");
+        assertEquals("ABC","d"); //replace d with expression
+    }
+
+    //skip one element
+    @Test
+    public void stream_3(){
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        List<Integer> integerListSkip;////here use skip and pass variable to assert
+        assertEquals(integerList.size()-1,2);
+
+    }
+
+    @Test
+    public void stream_4(){
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        assertEquals(true,false); //here use allMatch;
+
+    }
+
+    @Test
+    public void stream_5(){
+        List<Integer> integerList = Arrays.asList(8, 98, 312, 1);
+        assertEquals(true,false); //use Predicate<Integer> to pass this test with allMatch
+
+    }
+}
